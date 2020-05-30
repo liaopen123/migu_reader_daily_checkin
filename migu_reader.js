8db973a9e4fc8175d5dcfcdfdbfa413c90a86e23
liaopen123 <liaopenghui@haier.com>
2020-05-30 11:33:12 +0800

    Update migu_reader.js
    修改时间  添加阅读完后 回到首页

--- migu_reader.js
+++ migu_reader.js
@@ -12,43 +12,47 @@
 //     "手机型号: " + device.model + "\n" +
 //     "安卓版本: " + device.release);
 // 获取设备
-var height = device.height;
-var width = device.width;
-var readPageTime =1000*10;
+const height = device.height;
+const width = device.width;
+const readPageTime = 1000 * 10;
+const minute = 1000 * 60;
+const readMin = 15;//阅读时长
+const totalReadTime = minute * readMin;
 var appName = "咪咕阅读";
 launchApp(appName);
-//找到下面的5个tab的父控件
-// id("bottom_navigation_bar_item_container").findOne().children().forEach(function(child){
-//     log(child.className);
-// });
 sleep(6000);
-var container = id("bottom_navigation_bar_item_container").findOne();
-//tab
-container.child(0).click();
-// var booklist = id("local_listview").findOne();
-// if (booklist.children() > 0) {
-//     booklist.child(0).parent().parent().click();
-// } else {
-//     ShowMessage("请先添加书籍");
-// }
-
-
-
+turnTab2Home();
+//签到
 DoClickAction("签到领书券");
 sleep(2000);
+// if (checkExist("登录")) {
+//     ShowMessage("请先登录")
+// } else {
+// }
+ShowMessage("开始阅读...持续时长:" + readMin + "分钟");
 
-
-
-id("book_shelf_item_layout").findOne().click();
-
-var totalTime= 0;
-while(totalTime<=1000*60*15){
-    sleep(readPageTime/2);
-    swipe(width *0.8, height - 400, width *0.2, height-450, 1000);
-    sleep(readPageTime/2);
-    totalTime+=readPageTime;
-}
+openBookAndRead();
+back();
 ShowMessage("恭喜你阅读完成~");
+
+function turnTab2Home() {
+    var container = id("bottom_navigation_bar_item_container").findOne();
+    container.child(0).click();
+}
+function openBookAndRead() {
+    id("book_shelf_item_layout").findOne().click();
+
+    var totalTime = 0;
+    while (totalTime <= totalReadTime) {
+        sleep(readPageTime / 2);
+        swipe(width * 0.8, height - 400, width * 0.2, height - 450, 1000);
+        sleep(readPageTime / 2);
+        totalTime += readPageTime;
+    }
+
+}
+
+
 
 
 function ShowMessage(msg) {
@@ -57,15 +61,23 @@ function ShowMessage(msg) {
     sleep(1500);
 }
 
+//点击按钮不跳转的
+function DoClickAction(actionName) {
 
-function DoClickAction(actionName){
-    
     // ShowMessage("准备" + actionName)
     if (text(actionName).exists()) {
         text(actionName).findOne().click();
-        ShowMessage(actionName+"完成");
+        ShowMessage(actionName + "已点击");
     }
-    else{
+    else {
         ShowMessage("您已经" + actionName)
     }
+}
+
+
+
+
+function checkExist(actionName) {
+    return text(actionName).exists;
+
 }
